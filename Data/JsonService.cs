ee0211092df1c80c490e64ab9319f8f5433be942
Ademola Ogunkoya <ademolaogunkoya@gmail.comgit config --global user.name>
2023-05-17 16:08:22 +0100

    Initial0

---
+++ Data/JsonService.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http.Json;
+
+namespace Testa.Data
+{
+    public class JsonService
+    {
+        private readonly HttpClient http;
+
+        public JsonService(HttpClient http)
+        {
+            this.http = http;
+        }
+
+
+        public async Task<List<Person>> FetchData()
+        {
+            var items =await http.GetFromJsonAsync<Person[]>("sample-data/Members.json");
+            return items!.ToList();
+        }
+
+    }
+}
