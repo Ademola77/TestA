d50d583713a37f55c7e59d9fda7367c7f5ae99e6
Ademola Ogunkoya <ademolaogunkoya@gmail.comgit config --global user.name>
2023-05-17 17:04:27 +0100

    App0

--- Data/Person.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Testa.Data
-{
-    public class Person
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; } = string.Empty;
-        public string Surname { get; set; } = string.Empty;
-    }
-}
